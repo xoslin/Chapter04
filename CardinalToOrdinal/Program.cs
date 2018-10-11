b6f2e3f1683fde88510b68c111138154dc5da911
xoslin <xosevi_smr@hotmail.com>
2018-10-11 22:46:02 +0200

    Add project files.

---
+++ CardinalToOrdinal/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using static System.Console;
+
+namespace CardinalToOrdinal
+{
+    class Program
+    {
+        static string CardinalToOrdinal(int number)
+        {
+            switch (number)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return $"{number}th";
+                default:
+                    string numberAsText = number.ToString();
+                    char lastDigit = numberAsText[numberAsText.Length - 1];
+                    string suffix = string.Empty;
+                    switch (lastDigit)
+                    {
+                        case '1':
+                            suffix = "st";
+                            break;
+                        case '2':
+                            suffix = "nd";
+                            break;
+                        case '3':
+                            suffix = "rd";
+                            break;
+                        default:
+                            suffix = "th";
+                            break;
+                    }
+                    return $"{number}{suffix}";
+            }
+        }
+
+        static void RunCardinalToOrdinal()
+        {
+            for (int number = 1; number <= 40; number++)
+            {
+                Write($"{CardinalToOrdinal(number)} ");
+            }
+        }
+
+        static int Factorial(int number)
+        {
+            if (number < 1)
+            {
+                return 0;
+            }
+            else if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+
+        static void RunFactorial()
+        {
+            Write("Enter a number: ");
+            if(int.TryParse(ReadLine(), out int number))
+            {
+                WriteLine($"{number:N0}!={Factorial(number):N0}");
+            }
+            else
+            {
+                WriteLine("You did not enter a valid number!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //RunCardinalToOrdinal();
+            RunFactorial();
+            ReadKey();
+        }
+    }
+}
---
+++ WritingFunctions/Program.cs
@@ -0,0 +1,93 @@
+﻿using static System.Console;
+
+namespace WritingFunctions
+{
+    class Program
+    {
+        static void TimesTable(byte number)
+        {
+            WriteLine($"This is the {number} times table");
+            for (int row = 0; row <= 12; row++)
+            {
+                WriteLine($"{row} x {number} = {row * number})");
+            }
+        }
+
+        static void RunTimesTable()
+        {
+            Write("Enter a number between 0 and 255");
+            if(byte.TryParse(ReadLine(), out byte number))
+            {
+                TimesTable(number);
+            }
+            else
+            {
+                WriteLine("You did not enter a valid number!");
+            }
+        }
+
+        static decimal SalesTax(decimal amount, string twoLetterRegionCode)
+        {
+            decimal rate = 0.0M;
+            switch (twoLetterRegionCode)
+            {
+                case "CH": //Switzerland
+                    rate = 0.08M;
+                    break;
+                case "DK": //Denmark
+                case "NO": //Norway
+                    rate = 0.25M;
+                    break;
+                case "GB": //United Kingdom
+                case "FR": //France
+                    rate = 0.2M;
+                    break;
+                case "HU": //Hungary
+                    rate = 0.27M;
+                    break;
+                case "OR": //Oregon
+                case "AK": //Alaska
+                case "MT": //Montana
+                    rate = 0.0M;
+                    break;
+                case "ND": //North Dakota
+                case "WI": //Wisconsin
+                case "ME": //Maryland
+                case "VA": //Virginia
+                    rate = 0.05M;
+                    break;
+                case "CA": //California
+                    rate = 0.0825M;
+                    break;
+                default: //most US states
+                    rate = 0.06M;
+                    break;
+            }
+            return amount * rate;
+        }
+
+        static void RunSalesTax()
+        {
+            Write("Enter an amount: ");
+            string amountInText = ReadLine();
+            Write("Enter a two letter region code: ");
+            string region = ReadLine();
+            if(decimal.TryParse(amountInText, out decimal amount))
+            {
+                decimal taxToPay = SalesTax(amount, region);
+                WriteLine($"You must pay {taxToPay} in sales tax.");
+            }
+            else
+            {
+                WriteLine("You did not enter a valid amount!");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            //RunTimesTable();
+            RunSalesTax();
+            ReadKey();
+        }
+    }
+}
